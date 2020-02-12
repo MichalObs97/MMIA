bbccad9af9992d019187cf70609e6295bc7bc6c5
MichalObs97 <obsitnik.m@gmail.com>
2020-02-12 15:39:38 +0100

    4th

--- cv02/Src/main.c
+++ cv02/Src/main.c
@@ -12,7 +12,10 @@
 #endif
 
 #include "stm32f0xx.h"
+#define PIN_READ_TIME 1000
 #define LED_TIME_BLINK 300
+#define LED_TIME_SHORT 100
+#define LED_TIME_LONG 1000
 volatile uint32_t Tick;
 
 int main(void)
@@ -33,6 +36,7 @@ int main(void)
 	for (;;)
 		{
 		blikac();
+		tlacitka();
 		}
 }
 
@@ -61,3 +65,32 @@ void blikac(void)
  	 }
  }
 
+void tlacitka(void)
+{
+//static uint16_t debounce = 0xFFFF;
+static uint32_t old_s2;
+static uint32_t old_s1;
+static uint32_t off_time;
+static uint32_t delay;
+uint32_t new_s2 = GPIOC->IDR & (1<<0);
+uint32_t new_s1 = GPIOC->IDR & (1<<1);
+if (Tick > delay + PIN_READ_TIME) {
+//TLACITKO S2
+	if (old_s2 && !new_s2) { // falling edge
+		off_time = Tick + LED_TIME_SHORT;
+		GPIOB->BSRR = (1<<0);
+	 }
+  old_s2 = new_s2;
+
+//TLACITKO S1
+ 	 if (old_s1 && !new_s1) { // falling edge
+ 	 	off_time = Tick + LED_TIME_LONG;
+ 	 	GPIOB->BSRR = (1<<0);
+ 	 }
+   old_s1 = new_s1;
+}
+ 	 if (Tick > off_time) {
+ 	 	 GPIOB->BRR = (1<<0);
+ 	 	 }
+
+}
