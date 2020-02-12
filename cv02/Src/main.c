b55a1e29fac66588983dcf443f7c96f3f4c70689
MichalObs97 <obsitnik.m@gmail.com>
2020-02-12 14:42:24 +0100

    2nd

--- cv02/Src/main.c
+++ cv02/Src/main.c
@@ -12,38 +12,48 @@
 #endif
 
 #include "stm32f0xx.h"
+volatile uint32_t Tick;
 
 int main(void)
 {
-	//uint8_t pole[32] = {1,0,1,0,1,0,0,1,1,1,0,1,1,1,0,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,0,0};
-	//uint32_t first;
-	uint32_t sekvence = 0b10101001110111011100101010000000;
-	uint32_t index;
+	SysTick_Config(8000); // 1ms
+	RCC->AHBENR  |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN| RCC_AHBENR_GPIOCEN;    // enable
+	GPIOA->MODER |= GPIO_MODER_MODER4_0;                          					// LED1 = PA4, output
+	GPIOB->MODER |= GPIO_MODER_MODER0_0;                          					// LED2 = PB0, output
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR0_0;                         					// S2 = PC0, pullup
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0;                          					// S1 = PC1, pullup
 
-	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
-	GPIOA->MODER |= GPIO_MODER_MODER5_0;
-	GPIOA->BSRR = (1<<5); // set
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	EXTI->IMR |= EXTI_IMR_MR0; // mask
+	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
 
-//	first = sekvence;
-//        while (1) {
-//        	if (pole[index] == 1) {GPIOA->BSRR = (1<<5);} //set
-//        	else if (pole[index] == 0) {GPIOA->BRR = (1<<5);}  //reset
-//        	index++;                                      //increment
-//      	if (index >= sizeof(pole)) index=0;               //infinite loop of pole[32]
-//        	for (volatile uint32_t i = 0; i < 100000; i++) {}
-//	    }
-
-	index=0;
-
-	while (1) {
-		for (index=0; index < 32; index++){
-			if ((sekvence << index) & (0x80000000)) {
-				GPIOA->BSRR = (1<<5);   //set
-			}
-			else {
-				GPIOA->BRR = (1<<5);   //reset
-			}
-			for (volatile uint32_t i = 0; i < 100000; i++) {}
-		}
-	}
+	for (;;) {}
 }
+
+void EXTI0_1_IRQHandler(void)
+	 {
+     if (EXTI->PR & EXTI_PR_PR0)
+	 	 { 								// check line 0 has triggered the IT
+		 EXTI->PR |= EXTI_PR_PR0;       // clear the pending bit
+		 GPIOB-> ODR ^= (1<<0);   //set
+	 	 }
+	 }
+
+void SysTick_Handler(void)
+ {
+ Tick++;
+ }
+
+void blikac(void)
+ {
+ static uint32_t delay;
+
+ if (Tick > delay + LED_TIME_BLINK)
+ 	 {
+	 GPIOA->ODR ODR ^= (1<<0);
+	 delay = Tick;
+ 	 }
+ }
+
