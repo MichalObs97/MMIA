5d14b7b01d918f9b3360db3d7c406dde57934160
MichalObs97 <obsitnik.m@gmail.com>
2020-03-04 16:04:40 +0100

    2nd

--- cv05/Core/Src/main.c
+++ cv05/Core/Src/main.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Includes ------------------------------------------------------------------*/
@@ -23,7 +23,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include  <stdio.h>
+#include <string.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -33,6 +34,12 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
+#define RX_BUFFER_LEN 64
+#define CMD_BUFFER_LEN 256
+static uint8_t uart_rx_buf[RX_BUFFER_LEN];
+static volatile uint16_t uart_rx_read_ptr = 0;
+#define uart_rx_write_ptr (RX_BUFFER_LEN - hdma_usart2_rx.Instance->CNDTR)
+static uint16_t ls_char;
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -42,6 +49,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart2;
+DMA_HandleTypeDef hdma_usart2_rx;
 
 /* USER CODE BEGIN PV */
 
@@ -50,14 +58,82 @@ UART_HandleTypeDef huart2;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
-
+static void uart_byte_available(uint8_t c);
+static void uart_process_command(char *data);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+int _write(int file, char const *buf, int n)
+{
+	/* stdout redirection to UART2 */
+	HAL_UART_Transmit(&huart2, (uint8_t*)(buf), n, HAL_MAX_DELAY);
+	return n;
+}
 
+static void uart_byte_available(uint8_t c)
+{
+	static uint16_t cnt;
+	static char data[CMD_BUFFER_LEN];
+	if ((c>=32)&&(c<=126))
+	if (cnt < CMD_BUFFER_LEN) data[cnt++] = c;
+		if (c == '\n' || c == '\r') {
+
+			data[cnt] = '\0';
+			uart_process_command(data);
+			cnt = 0;
+			ls_char = HAL_GetTick();
+		}
+
+
+}
+
+
+static void uart_process_command(char *data)
+{
+	char *token;
+	token = strtok(data, " ");
+	if (strcasecmp(token, "HELLO") == 0) {
+		printf("Komunikace OK\n");
+	}
+	else if (strcasecmp(token, "LED1") == 0) {
+		token = strtok(NULL, " ");
+		if (strcasecmp(token, "ON") == 0)
+		{
+			HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 1);
+			printf("LED1 je zapnuta\n");
+		}
+		else if (strcasecmp(token, "OFF") == 0)
+		{
+			HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, 0);
+			printf("LED1 je vypnuta\n");
+		}
+	}
+	else if (strcasecmp(token, "LED2") == 0) {
+			token = strtok(NULL, " ");
+			if (strcasecmp(token, "ON") == 0)
+			{
+				HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 1);
+				printf("LED2 je zapnuta\n");
+			}
+			else if (strcasecmp(token, "OFF") == 0)
+			{
+				HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, 0);
+				printf("LED2 je vypnuta\n");
+			}
+		}
+	else if (strcasecmp(token, "STATUS") == 0)
+	{
+		if (HAL_GPIO_ReadPin(LED1_GPIO_Port, LED1_Pin) ==1 ) {printf("LED1 zapnuta\n");};
+		if (HAL_GPIO_ReadPin(LED1_GPIO_Port, LED1_Pin) ==0 ) {printf("LED1 vypnuta\n");};
+	    if (HAL_GPIO_ReadPin(LED2_GPIO_Port, LED2_Pin) ==1 ) {printf("LED2 zapnuta\n");};
+		if (HAL_GPIO_ReadPin(LED2_GPIO_Port, LED2_Pin) ==0 ) {printf("LED2 vypnuta\n");};
+
+	}
+}
 /* USER CODE END 0 */
 
 /**
@@ -89,21 +165,31 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
-
+	HAL_UART_Receive_DMA(&huart2, uart_rx_buf, RX_BUFFER_LEN);
   /* USER CODE END 2 */
  
  
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+	while (1)
+	{
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
+		//uint8_t c;
+		//HAL_UART_Receive(&huart2, &c, 1, HAL_MAX_DELAY);
+		//HAL_UART_Transmit(&huart2, &c, 1, HAL_MAX_DELAY);
+		while (uart_rx_read_ptr != uart_rx_write_ptr)
+		{
+			uint8_t b = uart_rx_buf[uart_rx_read_ptr];
+			if (++uart_rx_read_ptr >= RX_BUFFER_LEN) uart_rx_read_ptr = 0;    // increase read pointer
+			uart_byte_available(b);                                           // process every received byte with the RX state machine
+		}
+	}
   /* USER CODE END 3 */
 }
 
@@ -178,6 +264,22 @@ static void MX_USART2_UART_Init(void)
 
 }
 
+/** 
+  * Enable DMA controller clock
+  */
+static void MX_DMA_Init(void) 
+{
+
+  /* DMA controller clock enable */
+  __HAL_RCC_DMA1_CLK_ENABLE();
+
+  /* DMA interrupt init */
+  /* DMA1_Channel4_5_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel4_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel4_5_IRQn);
+
+}
+
 /**
   * @brief GPIO Initialization Function
   * @param None
@@ -191,9 +293,13 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED1_Pin|LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -201,12 +307,19 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  /*Configure GPIO pins : LED1_Pin LD2_Pin */
+  GPIO_InitStruct.Pin = LED1_Pin|LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LED2_Pin */
+  GPIO_InitStruct.Pin = LED2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
 
 }
 
@@ -221,7 +334,7 @@ static void MX_GPIO_Init(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
+	/* User can add his own implementation to report the HAL error return state */
 
   /* USER CODE END Error_Handler_Debug */
 }
@@ -237,7 +350,7 @@ void Error_Handler(void)
 void assert_failed(char *file, uint32_t line)
 { 
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
+	/* User can add his own implementation to report the file name and line number,
      tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
